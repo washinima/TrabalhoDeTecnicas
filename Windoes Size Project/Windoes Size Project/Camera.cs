1ac9d1d58f02b1d93e54e90c5d538302661539c6
whisp <washinima99@gmail.com>
2017-03-10 12:53:17 +0000

    Kill me pls
    o inicio da aula 9

---
+++ Windoes Size Project/Windoes Size Project/Camera.cs
@@ -0,0 +1,107 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Windoes_Size_Project
+{
+    public class Camera
+    {
+        static private Vector2 sOrigin = Vector2.Zero; // Origin of the world
+        static private float sWidth = 100f; // Width of the world
+        static private float sHeight = 56.25f; // Height of the world
+        static private float sRatio = -1f; // Ratio between camera window and pixel
+        static public Vector2 CameraWindowLowerLeftPosition
+        { get { return sOrigin; } }
+        static public Vector2 CameraWindowUpperRightPosition
+        { get { return sOrigin + new Vector2(sWidth, sHeight); } }
+
+
+        // Support collision with the camera bounds
+        public enum CameraWindowCollisionStatus
+        {
+            CollideTop = 0,
+            CollideBottom = 1,
+            CollideLeft = 2,
+            CollideRight = 3,
+            InsideWindow = 4
+        };
+
+
+        static private float cameraWindowToPixelRatio()
+        {
+            if (sRatio < 0f)
+                sRatio = (float)Game1.sGraphics.PreferredBackBufferWidth / sWidth;
+            return sRatio;
+        }
+
+        static public void SetCameraWindow(Vector2 origin, float width)
+        {
+            sOrigin = origin;
+            sWidth = width;
+        }
+
+        static public void ComputePixelPosition(Vector2 cameraPosition, out int x, out int y)
+        {
+            float ratio = cameraWindowToPixelRatio();
+
+            // Convert the position to pixel space
+            x = (int)(((cameraPosition.X - sOrigin.X) * ratio) + 0.5f);
+            y = (int)(((cameraPosition.Y - sOrigin.Y) * ratio) + 0.5f);
+
+            y = Game1.sGraphics.PreferredBackBufferHeight - y;
+        }
+
+        static public Rectangle ComputePixelRectangle(Vector2 position, Vector2 size)
+        {
+            float ratio = cameraWindowToPixelRatio();
+
+            // Convert size from camera window space to pixel space.
+            int width = (int)((size.X * ratio) + 0.5f);
+            int height = (int)((size.Y * ratio) + 0.5f);
+
+            // Convert the position to pixel space
+            int x, y;
+            ComputePixelPosition(position, out x, out y);
+
+            // Reference position is the center
+            y -= height / 2;
+            x -= width / 2;
+
+            return new Rectangle(x, y, width, height);
+        }
+
+        static public CameraWindowCollisionStatus CollidedWithCameraWindow(TexturedPrimitive prim)
+        {
+            Vector2 min = CameraWindowLowerLeftPosition;
+            Vector2 max = CameraWindowUpperRightPosition;
+            if (prim.MaxBound.Y > max.Y)
+                return CameraWindowCollisionStatus.CollideTop;
+            if (prim.MinBound.X < min.X)
+                return CameraWindowCollisionStatus.CollideLeft;
+            if (prim.MaxBound.X > max.X)
+                return CameraWindowCollisionStatus.CollideRight;
+            if (prim.MinBound.Y < min.Y)
+                return CameraWindowCollisionStatus.CollideBottom;
+            return CameraWindowCollisionStatus.InsideWindow;
+        }
+
+
+        public Vector2 RandomPosition(bool offCamera)
+        {
+            Vector2 position;
+            float posX = (float)Game1.sRan.NextDouble() * Camera.sWidth * 0.80f
+            + Camera.sWidth * 0.10f;
+            float posY = (float)Game1.sRan.NextDouble() * Camera.sHeight * 0.80f
+            + Camera.sHeight * 0.10f;
+            if (offCamera)
+                posX += Camera.CameraWindowUpperRightPosition.X;
+            position = new Vector2(posX, posY);
+            return position;
+        }
+
+
+    }
+}
